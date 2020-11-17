b60deff7526f5d0d16627cc79886c58fc2658efa
Enoch2k2 <enoch2k2@hotmail.com>
2020-11-17 12:25:38 -0600

    finished demo

---
+++ README.md
@@ -0,0 +1,12 @@
+## Introduction To OO Ruby
+
+# What is a Ruby Object?
+
+- object id
+- class name
+- methods
+- attributes
+
+# Why an object over a hash?
+
+Hashes are not actionable. Hashes can not store methods.
--- lib/models/pet.rb
+++ lib/models/pet.rb
@@ -1,3 +1,49 @@
 class Pet
+    @@all = []
+    attr_accessor :name, :age
+    # attr_writer :name, :age
+    attr_reader :species
+
+    # what is this? A lifecycle method.
+    # Pet.new => creates an instance and automatically calls initialize with that instance once created.
+    # Pet.new("Fido")
+    def initialize(name, age, species)
+        self.name = name
+        self.age = age
+        @species = species
+        @@all << self
+    end
+
+    # reader - reads the value
+    # def name
+    #     @name
+    # end
+
+    # writer - writes the value
+    # def name=(name)
+    #     @name = name
+    # end
+
+    # def species=(species)
+    #     @species = species
+    # end
+
+    # def species
+    #     @species
+    # end
+
+    # def age
+    #     @age
+    # end
+
+    # def age=(age)
+    #     @age = age
+    # end
+
+    # class Method
+    def self.all
+        @@all
+    end
 
 end
+
